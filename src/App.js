f0d54c2904d983ff032bd8fc8e4424696a330420
emad darabah <emad.dar97@gmail.com>
2019-09-22 17:26:29 +0300

    creating homepage components, styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './pages/home-page/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return <HomePage></HomePage>;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import Directory from '../../components/directory/directory.components';
+
+// styles
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
