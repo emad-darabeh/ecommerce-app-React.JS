13a946f903cbed6731ec7b5946ba5baeeebcb598
emad darabah <emad.dar97@gmail.com>
2019-09-23 03:09:14 +0300

    adding header component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+// components
+import Header from './components/header/header.component';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// style
+import './header.styles.scss';
+
+// logo svg
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to='/' className='logo-container'>
+        <Logo className='logo' />
+      </Link>
+
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
