018756ff283b7bae0bfd8bb6f32b49446042665d
emad darabah <emad.dar97@gmail.com>
2019-09-27 23:08:15 +0300

    added firebse.config file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6m-8vZLhsJ4FIDiInt9yOVGm2-UswzU4',
+  authDomain: 'e-commerce-app-1997.firebaseapp.com',
+  databaseURL: 'https://e-commerce-app-1997.firebaseio.com',
+  projectId: 'e-commerce-app-1997',
+  storageBucket: '',
+  messagingSenderId: '949564481776',
+  appId: '1:949564481776:web:2b60af1cb745c9ce9f6235'
+};
+
+export default firebaseConfig;
